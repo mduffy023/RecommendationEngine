c64925798a400fa60c61805ebb700c0aa6464a1e
martin duffy <martinduffy@hotmail.co.uk>
2024-08-19 19:53:27 +0100

    changes made to testEndpoint

--- UserInterface/EndPoints/TestEndpoint.cs
+++ UserInterface/EndPoints/TestEndpoint.cs
@@ -6,7 +6,7 @@ namespace front_end.EndPoints
 		public static void Map(WebApplication app)
 		{
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
-			app.MapGet("/yttrack/", (string? id, string? title, string? channelname, int? numoflikes) =>
+			app.MapGet("/yttrack/", (string? id, string? title, string? channelname, int? numoflikes, string? vidsrc) =>
 			{
 				return new TestReply() { Success = true };
 
