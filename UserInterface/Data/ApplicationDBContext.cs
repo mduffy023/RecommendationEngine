1d21a3c183602ab034fc6f962c14183f1c32161e
martin duffy <34552914+mduffy023@users.noreply.github.com>
2025-05-21 18:07:39 +0100

    added new class for DB

---
+++ UserInterface/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using front_end.EndPoints;
+
+
+namespace front_end.Data
+{
+    public class ApplicationDBContext : TestEndPoint
+    {
+        public ApplicationDBContext() 
+        {
+            
+        }
+    }
+}
